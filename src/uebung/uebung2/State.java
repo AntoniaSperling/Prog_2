62b1a94ae698f5c50991e504dcf9ac271c4ef0c7
AntoniaSperling <antonia.sperling@stundent.htw.-berlin.de>
2021-04-23 17:50:21 +0200

    Uebung 2 beendet

---
+++ src/uebung/uebung2/State.java
@@ -0,0 +1,3 @@
+package uebung.uebung2;
+
+public enum State { EMPTY, RED, BLACK }
--- src/uebung/uebung2/TicTacToe.java
+++ src/uebung/uebung2/TicTacToe.java
@@ -2,41 +2,139 @@ package uebung.uebung2;
 
 public class TicTacToe {
 
-	enum State {EMPTY, RED, BLACK};
 	State[][] field;
 
 	public TicTacToe()
 	{
 		field = new State[3][3];
-		for(int i=0; i<field.length; i++)
+		for(int i=0; i<this.field.length; i++)
 		{
-			for(int j=0; j<field[i].length; j++)
+			for(int j=0; j<this.field[i].length; j++)
 			{
 				field[i][j]=State.EMPTY;
 			}
 		}
 	}
 
-	public void makeMove(int i, int j, State player)
+	public void makeMove(int row, int col, State player)
 	{
-		if(field[i][j]==State.EMPTY && player!=State.EMPTY)   
-		{	field[i][j]=player;}
-	}
-
-	public void print(State player)
-	{
-		for(int index = 0; index< field.length; index++)
+		if(row>= 0 && row < this.field.length && col >= 0 && col < this.field[row].length && 
+				this.field[row][col] == State.EMPTY)
 		{
-			for(int i = 0; i < field[i].length; i++)
-			{
-				switch(player)
-				{
-				case EMPTY : System.out.print("-");
-				case RED   : System.out.print("X");
-				case BLACK : System.out.print("O");
-				}
-				System.out.println();
+			if(player!=State.EMPTY)   
+			{	
+				field[row][col]=player;
 			}
 		}
 	}
+
+	public void print()
+	{
+		for(int row = 0; row< field.length; row++)
+		{
+			for(int col = 0; col < field[row].length; col++)
+			{
+				if(field[row][col] == State.EMPTY)
+				{
+					System.out.print("- ");
+				}
+				else if(field[row][col] == State.RED)
+				{
+					System.out.print("x ");
+				}
+				else
+				{
+					System.out.print("o ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	/*public boolean gewonnen(State player)
+	{
+		if(player == State.EMPTY) return false;
+		
+		for(int row = 0; row < this.field.length; row++)
+		{
+			if(this.field[row][0] == player && this.field[row][1] == player && this.field[row][0] == player)
+			{
+				return true;
+			}
+		}
+		for(int col = 0; col < this.field.length; col++)
+		{
+			if(this.field[0][col] == player && this.field[1][col] == player && this.field[2][col] == player)
+			{
+				return true;
+			}
+		}
+		if(this.field[0][0]== player && this.field[1][1] == player && this.field[2][2] == player)
+		{
+			return true;
+		}
+		if(this.field[0][2]== player && this.field[1][1] == player && this.field[2][0] == player)
+		{
+			return true;
+		}
+
+			return false;
+	}*/
+	
+	  public boolean gewonnen(State player)
+	    {
+	        if(player == State.EMPTY) return false;
+
+	        // alle drei Zeilen pruefen
+	        for(int row=0; row<this.field.length; row++)
+	        {
+	            if(this.field[row][0] == player && this.field[row][1] == player && this.field[row][2] == player)
+	            {
+	                return true;
+	            }
+	        }
+
+	        // alle drei Spalten pruefen
+	        for(int col=0; col<this.field.length; col++)
+	        {
+	            if(this.field[0][col] == player && this.field[1][col] == player && this.field[2][col] == player)
+	            {
+	                return true;
+	            }
+	        }
+
+	        // Diagonale von links oben nach rechts unten
+	        if(this.field[0][0] == player && this.field[1][1] == player && this.field[2][2] == player)
+	        {
+	            return true;
+	        }
+
+	        // Diagonale von rechts oben nach links unten
+	        if(this.field[0][2] == player && this.field[1][1] == player && this.field[2][0] == player)
+	        {
+	            return true;
+	        }
+	        return false;
+	    }
+	
+	public boolean voll()
+	{
+		for(int row = 0; row < this.field.length; row++)
+		{
+			for(int col = 0; col < this.field[row].length; col++)
+			{
+				if(this.field[row][col]== State.EMPTY)
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public boolean unentschieden()
+	{
+		return (this.voll()&& ! this.gewonnen(State.BLACK)&& !this.gewonnen(State.RED));
+	}
+	
 }
